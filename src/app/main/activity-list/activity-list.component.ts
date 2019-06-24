1b1de081768e5634a3311b16a9d326477a5519d9
Yu, Qihui <Qihui.Yu@misys.com>
2019-06-24 18:31:07 +0200

    FLG-012: Add questions; try to refactor questions classes

--- src/app/main/activity-list/activity-list.component.ts
+++ src/app/main/activity-list/activity-list.component.ts
@@ -5,6 +5,7 @@ import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { CategoriesService } from '../categories.service';
 import { Observable } from 'rxjs';
+import { ActivitiesService } from '../activities.service';
 
 @Component({
   selector: 'app-activity-list',
@@ -13,53 +14,15 @@ import { Observable } from 'rxjs';
 })
 export class ActivityListComponent implements OnInit {
 
-  category0: Category;
   category$: Observable<Category>;
-  activities: Activity[];
+  activities$: Observable<Activity[]>;
 
   constructor(  
     private route: ActivatedRoute,
     private router: Router,
-    private service: CategoriesService
+    private categoriesService: CategoriesService,
+    private activitiesService: ActivitiesService
     ) { 
-    this.activities = [
-      {
-        name: "Aller à la Poste",
-        category: this.category0,
-        difficulty: Difficulty.DÉBUTANT,
-        unlocked: true
-      },
-      {
-        name: "Apporter ses documents",
-        category: this.category0,
-        difficulty: Difficulty.DÉBUTANT,
-        unlocked: true
-      },
-      {
-        name: "Comprendre un message d'annulation",
-        category: this.category0,
-        difficulty: Difficulty.DÉBUTANT,
-        unlocked: true
-      },
-      {
-        name: "Comprendre une heure de rendez-vous",
-        category: this.category0,
-        difficulty: Difficulty.INTERMÉDIAIRE,
-        unlocked: true
-      },
-      {
-        name: "Envoyer un courrier",
-        category: this.category0,
-        difficulty: Difficulty.DÉBUTANT,
-        unlocked: true
-      },
-      {
-        name: "Identifier la date de fin de validité d'un document",
-        category: this.category0,
-        difficulty: Difficulty.INTERMÉDIAIRE,
-        unlocked: true
-      },
-    ]
 
     
   }
@@ -67,7 +30,13 @@ export class ActivityListComponent implements OnInit {
   ngOnInit() {
     this.category$ = this.route.paramMap.pipe(
       switchMap((params: ParamMap) =>
-        this.service.getCategory(params.get('id'))
+        this.categoriesService.getCategory(params.get('id'))
+      )
+    );
+
+    this.activities$ = this.route.paramMap.pipe(
+      switchMap((params: ParamMap) =>
+        this.activitiesService.getActivities(params.get('id'))
       )
     );
   }
