b7345f759cb887bc6d0f2519ac386e66cb45bfa3
Yu, Qihui <Qihui.Yu@misys.com>
2019-07-08 17:42:02 +0200

    FLG-022: Refacotr nav components

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+  navItems = [
+    {
+      name: 'Main',
+      link: '/main',
+      icon: 'face'
+    },
+    {
+      name: 'Progress',
+      link: '/progress',
+      icon: 'trending_up'
+    },    
+    {
+      name: 'Vocabulary',
+      link: '/vocabulary',
+      icon: 'ballot'
+    },    
+    {
+      name: 'Settings',
+      link: '/settings',
+      icon: 'settings'
+    },
+  ]
+
+  activeNavItem = this.navItems[0];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onClick(item) {
+    this.activeNavItem = item;
   }
 
 }
--- src/assets/vocabulary.ts
+++ src/assets/vocabulary.ts
@@ -16,71 +16,7 @@ export const VOCABULARY_1: Vocabulary[] = [
     audioSrc: "string",
     explanation: "string",
     usage: "string"
-},
-{
-    id: 103,
-    name: "string",
-    categoryId: 1,
-    audioSrc: "string",
-    explanation: "string",
-    usage: "string"
-},
-{
-    id: 104,
-    name: "string",
-    categoryId: 1,
-    audioSrc: "string",
-    explanation: "string",
-    usage: "string"
-},
-{
-    id: 105,
-    name: "string",
-    categoryId: 1,
-    audioSrc: "string",
-    explanation: "string",
-    usage: "string"
-},
-{
-    id: 106,
-    name: "string",
-    categoryId: 1,
-    audioSrc: "string",
-    explanation: "string",
-    usage: "string"
-},
-{
-    id: 107,
-    name: "string",
-    categoryId: 1,
-    audioSrc: "string",
-    explanation: "string",
-    usage: "string"
-},
-{
-    id: 108,
-    name: "string",
-    categoryId: 1,
-    audioSrc: "string",
-    explanation: "string",
-    usage: "string"
-},
-{
-    id: 109,
-    name: "string",
-    categoryId: 1,
-    audioSrc: "string",
-    explanation: "string",
-    usage: "string"
-},
-{
-    id: 110,
-    name: "string",
-    categoryId: 1,
-    audioSrc: "string",
-    explanation: "string",
-    usage: "string"
-},
+}
 ]
 
 export const VOCABULARY_2: Vocabulary[] = [
