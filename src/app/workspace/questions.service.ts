e64d78f6547c9adbe478a3b8fd38c01ce74bfee2
Yu, Qihui <qihui.yu@finastra.com>
2019-07-10 10:23:25 +0200

    FLE-021: Minor fix

--- src/app/workspace/questions.service.ts
+++ src/app/workspace/questions.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { QSET } from 'src/assets/activities';
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { QCMSelectText, QCMSelectImage, AssociateTextToImage, AssociateImageToGoal, CardsortingTextTo2Goals, Question, SelectOnCalendar } from '../domain/question';
+import { QCMSelectText, QCMSelectImage, AssociateTextToImage, AssociateImageToGoal, Question, SelectOnCalendar } from '../domain/question';
 
 @Injectable({
   providedIn: 'root'
