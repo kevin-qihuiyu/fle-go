b6fc2aeb9f596a5e4fc2ba2d32e5cfaad822ce67
kevin-qihuiyu <imposiwind@gmail.com>
2019-07-30 23:37:57 +0200

    FLE-040: Publish: Cleanup for MVP; Data enrichement for questions

--- docs/assets/activity-list.ts
+++ docs/assets/activity-list.ts
@@ -4,7 +4,7 @@ import { Difficulty } from 'src/app/domain/activity';
 // Cat1: Vie quotidienne
 export const ACTIVITIES_1: Activity[] = [
     {
-      name: "Acheter des chaussures",
+      name: "Connaître les noms de vêtements",
       categoryId: 1,
       difficulty: Difficulty.DÉBUTANT,
       questionId: 101
@@ -16,7 +16,13 @@ export const ACTIVITIES_1: Activity[] = [
       questionId: 102
     },
     {
-      name: "Connaître les noms de vêtements",
+      name: "Acheter des chaussures",
+      categoryId: 3,
+      difficulty: Difficulty.DÉBUTANT,
+      questionId: 101
+    },
+    {
+      name: "Identifier un moyen de paiement",
       categoryId: 1,
       difficulty: Difficulty.DÉBUTANT,
       questionId: 104
@@ -40,64 +46,40 @@ export const ACTIVITIES_1: Activity[] = [
       questionId: 107
     },
     {
-      name: "Identifier un moyen de paiement",
+      name: "Identifier la cuisine française",
       categoryId: 1,
       difficulty: Difficulty.DÉBUTANT,
-      questionId: 110
-    },
-    {
-      name: "Retirer un colis",
-      categoryId: 1,
-      difficulty: Difficulty.DÉBUTANT,
-      questionId: 110
+      questionId: 108
     },
     {
       name: "Identifier la cuisine française",
       categoryId: 1,
       difficulty: Difficulty.DÉBUTANT,
-      questionId: 111
-    },
-    {
-      name: "Identifier la cuisine française",
-      categoryId: 1,
-      difficulty: Difficulty.DÉBUTANT,
-      questionId: 112
+      questionId: 109
     },
     {
       name: "Choisir la cuisson",
       categoryId: 1,
       difficulty: Difficulty.DÉBUTANT,
-      questionId: 113
-    },
-    {
-      name: "Choisir la garniture",
-      categoryId: 1,
-      difficulty: Difficulty.DÉBUTANT,
-      questionId: 106
-    },
-    {
-      name: "Choisir les parfums de glace",
-      categoryId: 1,
-      difficulty: Difficulty.DÉBUTANT,
-      questionId: 106
+      questionId: 110
     },
     // {
-    //   name: "À la boulangerie",
+    //   name: "Choisir la garniture",
     //   categoryId: 1,
     //   difficulty: Difficulty.DÉBUTANT,
-    //   questionId: 108
+    //   questionId: 106
     // },
     // {
-    //   name: "Identifier les catégories de produits",
+    //   name: "Retirer un colis",
     //   categoryId: 1,
     //   difficulty: Difficulty.DÉBUTANT,
-    //   questionId: 109
+    //   questionId: 110
     // },
     // {
-    //   name: "Acheter un billet de train",
+    //   name: "Choisir les parfums de glace",
     //   categoryId: 1,
     //   difficulty: Difficulty.DÉBUTANT,
-    //   questionId: 103
+    //   questionId: 106
     // },
 
   ]
@@ -127,26 +109,26 @@ export const ACTIVITIES_2: Activity[] = [
       name: "Identifier la date de fin de validité d'un document",
       categoryId: 2,
       difficulty: Difficulty.DÉBUTANT,
-      questionId: 205
+      questionId: 204
     },
     {
       name: "Identifier un courrier administratif",
       categoryId: 2,
       difficulty: Difficulty.DÉBUTANT,
-      questionId: 206
+      questionId: 205
     },
-    {
-      name: "Se repérer à la préfecture",
-      categoryId: 2,
-      difficulty: Difficulty.DÉBUTANT,
-      questionId: 206
-    },
-    {
-      name: "Remplir un formulaire de CAF",
-      categoryId: 2,
-      difficulty: Difficulty.DÉBUTANT,
-      questionId: 206
-    },
+    // {
+    //   name: "Se repérer à la préfecture",
+    //   categoryId: 2,
+    //   difficulty: Difficulty.DÉBUTANT,
+    //   questionId: 206
+    // },
+    // {
+    //   name: "Remplir un formulaire de CAF",
+    //   categoryId: 2,
+    //   difficulty: Difficulty.DÉBUTANT,
+    //   questionId: 207
+    // },
     // {
     //     name: "Lire un texto",
     //     categoryId: 2,
@@ -194,52 +176,58 @@ export const ACTIVITIES_3: Activity[] = [
     questionId: 302
   },
   {
-    name: "Prendre le bus",
+    name: "Moyen de transport",
     categoryId: 3,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 301
+    questionId: 303
   },
-  {
-    name: "Prendre le métro",
-    categoryId: 3,
-    difficulty: Difficulty.DÉBUTANT,
-    questionId: 302
-  },
+  // {
+  //   name: "Prendre le bus",
+  //   categoryId: 3,
+  //   difficulty: Difficulty.DÉBUTANT,
+  //   questionId: 301
+  // },
+  // {
+  //   name: "Prendre le métro",
+  //   categoryId: 3,
+  //   difficulty: Difficulty.DÉBUTANT,
+  //   questionId: 302
+  // },
   // train avec station non desservi; 
   // plusieur directions
   // Incident voyageur; Colis suspect; malaise voyageur
   // Station fermer suite à manifestation
-  {
-    name: "Prendre le métro",
-    categoryId: 3,
-    difficulty: Difficulty.DÉBUTANT,
-    questionId: 302
-  },
-  {
-    name: "Comprendre des indications sur les transports",
-    categoryId: 3,
-    difficulty: Difficulty.DÉBUTANT,
-    questionId: 302
-  },
-  {
-    name: "Prendre le train",
-    categoryId: 3,
-    difficulty: Difficulty.DÉBUTANT,
-    questionId: 302
-  },
-  {
-    name: "Prendre l'avion",
-    categoryId: 3,
-    difficulty: Difficulty.DÉBUTANT,
-    questionId: 302
-  },
+  // {
+  //   name: "Prendre le métro",
+  //   categoryId: 3,
+  //   difficulty: Difficulty.DÉBUTANT,
+  //   questionId: 302
+  // },
+  // {
+  //   name: "Comprendre des indications sur les transports",
+  //   categoryId: 3,
+  //   difficulty: Difficulty.DÉBUTANT,
+  //   questionId: 302
+  // },
+  // {
+  //   name: "Prendre le train",
+  //   categoryId: 3,
+  //   difficulty: Difficulty.DÉBUTANT,
+  //   questionId: 302
+  // },
+  // {
+  //   name: "Prendre l'avion",
+  //   categoryId: 3,
+  //   difficulty: Difficulty.DÉBUTANT,
+  //   questionId: 302
+  // },
   // Côntroleur
-  {
-    name: "Montrer son titre de transport",
-    categoryId: 3,
-    difficulty: Difficulty.DÉBUTANT,
-    questionId: 302
-  },
+  // {
+  //   name: "Montrer son titre de transport",
+  //   categoryId: 3,
+  //   difficulty: Difficulty.DÉBUTANT,
+  //   questionId: 302
+  // },
 ]
 
 
@@ -261,19 +249,19 @@ export const ACTIVITIES_4: Activity[] = [
       name: "Comprendre l'information sur le logement",
       categoryId: 4,
       difficulty: Difficulty.DÉBUTANT,
-      questionId: 402
+      questionId: 403
   },
   {
       name: "Lire une annonce immobilière",
       categoryId: 4,
       difficulty: Difficulty.DÉBUTANT,
-      questionId: 402
+      questionId: 404
   },
   {
       name: "Lire un contrat de location",
       categoryId: 4,
       difficulty: Difficulty.DÉBUTANT,
-      questionId: 402
+      questionId: 405
   },
 ]
 
@@ -283,37 +271,37 @@ export const ACTIVITIES_5: Activity[] = [
     name: "Assister à une soirée",
     categoryId: 5,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 502
+    questionId: 501
   },
   {
     name: "Aller au cinéma",
     categoryId: 5,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 501
+    questionId: 502
   },
   {
     name: "Visiter un musée",
     categoryId: 5,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 501
+    questionId: 503
   },
   {
     name: "Aller à la piscine",
-    categoryId: 6,
+    categoryId: 5,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 504
   },
   {
     name: "À la station de ski",
-    categoryId: 6,
+    categoryId: 5,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 505
   },
   {
     name: "Au poney club",
-    categoryId: 6,
+    categoryId: 5,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 506
   },
 ]
 
@@ -335,80 +323,80 @@ export const ACTIVITIES_6: Activity[] = [
     name: "Comprendre les consignes du médecin",
     categoryId: 6,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 601
+    questionId: 603
   },
   {
     name: "Dire un symptôme",
     categoryId: 6,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 604
   },
   {
     name: "Identifier le tarif d'une consultation",
     categoryId: 6,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 605
   },
   {
     name: "Identifier une feuille de soins",
     categoryId: 6,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 606
   },
   {
     name: "Lire une ordonnance",
     categoryId: 6,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 607
   },
   {
     name: "À la pharmacie",
     categoryId: 6,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 608
   },
   {
     name: "Présentater la carte Vitale",
     categoryId: 6,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 609
   },
   {
     name: "Visite chez l'ophtalmologue",
     categoryId: 6,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 610
   },
   {
     name: "Visite chez le dentiste",
     categoryId: 6,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 611
   },
   {
     name: "Visite chez une laboratoire médicale",
     categoryId: 6,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 612
   },
   {
     name: "Aller à la gym",
     categoryId: 6,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 602
+    questionId: 613
   },
 ]
 
 // Cat7: Vie amoureuse
 export const ACTIVITIES_7: Activity[] = [
   {
-      name: "Moyen de transport",
+      name: "Draguer une fille",
       categoryId: 7,
       difficulty: Difficulty.DÉBUTANT,
       questionId: 701
   },
   {
-      name: "Moyen de transport",
+      name: "Refuser un homme",
       categoryId: 7,
       difficulty: Difficulty.DÉBUTANT,
       questionId: 702
@@ -433,6 +421,6 @@ export const ACTIVITIES_8: Activity[] = [
     name: "Entretien",
     categoryId: 8,
     difficulty: Difficulty.DÉBUTANT,
-    questionId: 802
+    questionId: 803
   },
 ]
