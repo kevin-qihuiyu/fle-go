4385498e1553122893c59fa36e7b2bb054c835a7
Yu, Qihui <qihui.yu@finastra.com>
2019-08-07 11:36:18 +0200

    FLE-053: Make progress persisant work for user

--- src/app/_models/user.ts
+++ src/app/_models/user.ts
@@ -9,4 +9,9 @@
     cat1doneQuestionIds: string[];
     cat2doneQuestionIds: string[];
     cat3doneQuestionIds: string[];
+    cat4doneQuestionIds: string[];
+    cat5doneQuestionIds: string[];
+    cat6doneQuestionIds: string[];
+    cat7doneQuestionIds: string[];
+    cat8doneQuestionIds: string[];
 }
--- src/app/progress/progress.service.ts
+++ src/app/progress/progress.service.ts
@@ -8,7 +8,8 @@ import { first } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class ProgressService {
-  currentUser
+  currentUser;
+  currentUserSnapshot;
 
   progressData = [
     {
@@ -55,12 +56,16 @@ export class ProgressService {
 
   constructor(private authenticationService: AuthService,
     private userService: UserService) { 
-    // this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
-    this.currentUser = this.authenticationService.currentUserValue;
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
 
     this.progressData[0].doneQuestionIds = this.currentUser.cat1doneQuestionIds;
     this.progressData[1].doneQuestionIds = this.currentUser.cat2doneQuestionIds;
     this.progressData[2].doneQuestionIds = this.currentUser.cat3doneQuestionIds;
+    this.progressData[3].doneQuestionIds = this.currentUser.cat4doneQuestionIds;
+    this.progressData[4].doneQuestionIds = this.currentUser.cat5doneQuestionIds;
+    this.progressData[5].doneQuestionIds = this.currentUser.cat6doneQuestionIds;
+    this.progressData[6].doneQuestionIds = this.currentUser.cat7doneQuestionIds;
+    this.progressData[7].doneQuestionIds = this.currentUser.cat8doneQuestionIds;
   }
 
   getAllProgress() {
@@ -82,39 +87,81 @@ export class ProgressService {
   }
 
   addDoneQuestionsWithId(id: string) {
-    let index = +id.charAt(0) -1; // hardcoded solution: first digit of qid indicates category
-    
-    // if (!this.progressData[index].doneQuestionIds.includes(id))
-    //   this.progressData[index].doneQuestionIds.push(id);
+    let index = +id.charAt(0); 
+    // hardcoded solution: first digit of qid indicates category; eg qid-104 is a question in category1
 
-    //update database via api
     switch(index) {
-      case 0: { 
+      case 1: { 
         if (!this.currentUser.cat1doneQuestionIds.includes(id)) {
           this.currentUser.cat1doneQuestionIds.push(id);
-          this.userService.update(this.currentUser._id, this.currentUser)
-          .pipe(first())
-          .subscribe();
+          this.updateUser(this.currentUser);
         }
         break; 
       }
+      case 2: { 
+        if (!this.currentUser.cat2doneQuestionIds.includes(id)) {
+          this.currentUser.cat2doneQuestionIds.push(id);
+          this.updateUser(this.currentUser);
+        }
+        break; 
+      }
+      case 3: { 
+        if (!this.currentUser.cat3doneQuestionIds.includes(id)) {
+          this.currentUser.cat3doneQuestionIds.push(id);
+          this.updateUser(this.currentUser);
+        }
+        break; 
+      }
+      case 4: { 
+        if (!this.currentUser.cat4doneQuestionIds.includes(id)) {
+          this.currentUser.cat4doneQuestionIds.push(id);
+          this.updateUser(this.currentUser);
+        }
+        break; 
+      }
+      case 5: { 
+        if (!this.currentUser.cat5doneQuestionIds.includes(id)) {
+          this.currentUser.cat5doneQuestionIds.push(id);
+          this.updateUser(this.currentUser);
+        }
+        break; 
+      }
+      case 6: { 
+        if (!this.currentUser.cat6doneQuestionIds.includes(id)) {
+          this.currentUser.cat6doneQuestionIds.push(id);
+          this.updateUser(this.currentUser);
+        }
+        break; 
+      }
+      case 7: { 
+        if (!this.currentUser.cat7doneQuestionIds.includes(id)) {
+          this.currentUser.cat7doneQuestionIds.push(id);
+          this.updateUser(this.currentUser);
+        }
+        break; 
+      }
+      case 8: { 
+        if (!this.currentUser.cat8doneQuestionIds.includes(id)) {
+          this.currentUser.cat8doneQuestionIds.push(id);
+          this.updateUser(this.currentUser);
+        }
+        break; 
+      }
+    }
+  }
 
-
-     } 
-    //   case 1: { 
-    //     this.currentUser.cat2doneQuestionIds.push(id);
-    //     this.userService.update(this.currentUser.id, this.currentUser);
-    //     break; 
-    //  } 
-    //   case 2: { 
-    //     this.currentUser.cat3doneQuestionIds.push(id);
-    //     this.userService.update(this.currentUser.id, this.currentUser);
-    //     break; 
-    //  } 
+  updateUser(user) {
+    // update database via api
+    this.userService.update(user._id, user)
+      .pipe(first())
+      .subscribe();
+    // update current user store in local storage (session)
+    localStorage.setItem('currentUser', JSON.stringify(user));
   }
 
   clearAllDoneQuestions() {
     this.progressData.forEach(progress => progress.doneQuestionIds = [])
+    //TODO: use API to update database
   }
 
 
