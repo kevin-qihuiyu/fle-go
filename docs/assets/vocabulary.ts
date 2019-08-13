b31ab008b3ccf1a490e78f6d88b68369d1bdd8bf
Yu, Qihui <qihui.yu@finastra.com>
2019-08-13 17:56:22 +0200

    FLE-069: Database enrichment for vaocabulary

--- docs/assets/vocabulary.ts
+++ docs/assets/vocabulary.ts
@@ -2,297 +2,306 @@ import { Vocabulary } from '@/_models/vocabulary';
 
 // Cat1: Vie quotidienne
 export const VOCABULARY_1: Vocabulary[] = [
-{
-    id: 101,
-    name: "une livraison",
-    categoryId: 1,
-    audioSrc: "assets/vocabulary/1/101.mp3",
-    attr: "n.",
-    explanation: "Quand on a fait les courses par exemple sur Internet et que le magasin les apporte directment à la maison",
-    usage: "Ouvre la porte, c'est la livraison de courses."
-},
-{
-    id: 102,
-    name: "un croissant",
-    categoryId: 1,
-    audioSrc: "",
-    attr: "n.",
-    explanation: "Un gâteau qui se mange au petit déjeuner et qui a la forme de la lune.",
-    usage: "Un café et croissant s'il vous plaît!"
-},
-{
-    id: 103,
-    name: "une casquette",
-    categoryId: 1,
-    audioSrc: "",
-    attr: "n.",
-    explanation: "On met une casquette usr la tête pour se protéger du soleil",
-    usage: "N'oublie pas ta casquette pour aller pique-niquer."
-},
-{
-    id: 104,
-    name: "une liste de courses",
-    categoryId: 1,
-    audioSrc: "",
-    attr: "n.",
-    explanation: "Un papier où on écrit tout ce qu'on veux acheter avant d'aller dans le magasin.",
-    usage: "N'oublie pas de metter beurre et jus d'orange sue ta liste avant d'aller au supermarché."
-},
-{
-    id: 105,
-    name: "un rayon",
-    categoryId: 1,
-    audioSrc: "",
-    attr: "n.",
-    explanation: "Dans un supermarché, on trouve plusieurs rayons: fruits et légumes, viande, épicerie, etc",
-    usage: "Le coca? C'est au rayon boisson."
-},
-{
-    id: 106,
-    name: "une étiquette",
-    categoryId: 1,
-    audioSrc: "",
-    attr: "n.",
-    explanation: "Elle est collée aux produits ou arrochée aux vêtements, il y a le prix dessus.",
-    usage: "Je vais couper l'étiquette pour mettre cette chemise."
-},
-{
-    id: 107,
-    name: "la garniture",
-    categoryId: 1,
-    audioSrc: "",
-    attr: "n.",
-    explanation: "Aliments que l'on ajoute à un plat avant de le servir pour le compléter ou pour le décorer.",
-    usage: "On prends les frites et les jeune pousse comme garniture."
-},
-{
-    id: 108,
-    name: "un abonnement",
-    categoryId: 1,
-    audioSrc: "",
-    attr: "n.",
-    explanation: "Accord conventionnel de fourniture régulière (d'un produit ou d'un service)",
-    usage: "Je dois résilier l'abonnement Internet de mon ancien appartement."
-},
-{
-    id: 109,
-    name: "un forfait",
-    categoryId: 1,
-    audioSrc: "",
-    attr: "n.",
-    explanation: "Ensemble des prestations et des services pour un prix déterminé",
-    usage: "Les forfaits sans engagement free : Internet 4G, SMS/MMS illimités, 100% en ligne."
-},
-{
-    id: 110,
-    name: "un engagement",
-    categoryId: 1,
-    audioSrc: "",
-    attr: "n.",
-    explanation: "Promesse ou contrat pour une durée déterminé. Si on résilie un service en engagement avant, cela engendrera des frais.",
-    usage: "Les forfaits sans engagement free : Internet 4G, SMS/MMS illimités, 100% en ligne."
-},
+    {
+        id: 101,
+        name: "une livraison",
+        categoryId: 1,
+        audioSrc: "assets/vocabulary/1/101.mp3",
+        attr: "n.",
+        explanation: "Quand on a fait les courses par exemple sur Internet et que le magasin les apporte directment à la maison",
+        usage: "Ouvre la porte, c'est la livraison de courses."
+    },
+    {
+        id: 102,
+        name: "un croissant",
+        categoryId: 1,
+        audioSrc: "",
+        attr: "n.",
+        explanation: "Un gâteau qui se mange au petit déjeuner et qui a la forme de la lune.",
+        usage: "Un café et croissant s'il vous plaît!"
+    },
+    {
+        id: 103,
+        name: "une casquette",
+        categoryId: 1,
+        audioSrc: "",
+        attr: "n.",
+        explanation: "On met une casquette usr la tête pour se protéger du soleil",
+        usage: "N'oublie pas ta casquette pour aller pique-niquer."
+    },
+    {
+        id: 104,
+        name: "une liste de courses",
+        categoryId: 1,
+        audioSrc: "",
+        attr: "n.",
+        explanation: "Un papier où on écrit tout ce qu'on veux acheter avant d'aller dans le magasin.",
+        usage: "N'oublie pas de metter beurre et jus d'orange sue ta liste avant d'aller au supermarché."
+    },
+    {
+        id: 105,
+        name: "un rayon",
+        categoryId: 1,
+        audioSrc: "",
+        attr: "n.",
+        explanation: "Dans un supermarché, on trouve plusieurs rayons: fruits et légumes, viande, épicerie, etc",
+        usage: "Le coca? C'est au rayon boisson."
+    },
+    {
+        id: 106,
+        name: "une étiquette",
+        categoryId: 1,
+        audioSrc: "",
+        attr: "n.",
+        explanation: "Elle est collée aux produits ou arrochée aux vêtements, il y a le prix dessus.",
+        usage: "Je vais couper l'étiquette pour mettre cette chemise."
+    },
+    {
+        id: 107,
+        name: "la garniture",
+        categoryId: 1,
+        audioSrc: "",
+        attr: "n.",
+        explanation: "Aliments que l'on ajoute à un plat avant de le servir pour le compléter ou pour le décorer.",
+        usage: "On prends les frites et les jeune pousse comme garniture."
+    },
+    {
+        id: 108,
+        name: "un abonnement",
+        categoryId: 1,
+        audioSrc: "",
+        attr: "n.",
+        explanation: "Accord conventionnel de fourniture régulière (d'un produit ou d'un service)",
+        usage: "Je dois résilier l'abonnement Internet de mon ancien appartement."
+    },
+    {
+        id: 109,
+        name: "un forfait",
+        categoryId: 1,
+        audioSrc: "",
+        attr: "n.",
+        explanation: "Ensemble des prestations et des services pour un prix déterminé",
+        usage: "Les forfaits sans engagement free : Internet 4G, SMS/MMS illimités, 100% en ligne."
+    },
+    {
+        id: 110,
+        name: "un engagement",
+        categoryId: 1,
+        audioSrc: "",
+        attr: "n.",
+        explanation: "Promesse ou contrat pour une durée déterminé. Si on résilie un service en engagement avant, cela engendrera des frais.",
+        usage: "Les forfaits sans engagement free : Internet 4G, SMS/MMS illimités, 100% en ligne."
+    },
 
 ]
 
 // Cat2: Administration
 export const VOCABULARY_2: Vocabulary[] = [
-{
-    id: 201,
-    name: "télécharger",
-    categoryId: 1,
-    attr: "v.",
-    audioSrc: "",
-    explanation: "Quand on a un document sur internet et qu'on le met sur notre ordinateur, notre tablette ou notre téléphone.",
-    usage: "Téléchargez le document puis signez-le et renvoyez-le moi."
-},
-{
-    id: 202,
-    name: "un courrier",
-    categoryId: 1,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "Une lettre ou des documents qui arrivent par la poste.",
-    usage: "Tu dois envoyer ce courrier avant le 18 mai."
-},
-{
-    id: 203,
-    name: "un timbre",
-    categoryId: 1,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "On le colle sur une enveloppe ou sur un document officiel.",
-    usage: "Sur ce document il faut coller un timbre fiscal."
-},
-{
-    id: 204,
-    name: "un dossier",
-    categoryId: 1,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "Une pochette en carton ou en plastique pour ranger ensemble plusieur papiers et documents",
-    usage: "Tous les documents de la banque sont dans le même dossier, le bleu."
-},
-{
-    id: 205,
-    name: "un portefeuille",
-    categoryId: 1,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "Un petit sac pour ranger et transporter ses papier, ses cartes",
-    usage: "Mes photos d'identité sont dans mon portefeuille."
-},
-{
-    id: 206,
-    name: "un agenda",
-    categoryId: 1,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "Un cahier ou sur un téléphone. Il y a les jours et les dates, et ça sert à noter des rendez-vous.",
-    usage: "Elle note le rendez-vous sur son agenda."
-},
+    {
+        id: 201,
+        name: "télécharger",
+        categoryId: 1,
+        attr: "v.",
+        audioSrc: "",
+        explanation: "Quand on a un document sur internet et qu'on le met sur notre ordinateur, notre tablette ou notre téléphone.",
+        usage: "Téléchargez le document puis signez-le et renvoyez-le moi."
+    },
+    {
+        id: 202,
+        name: "un courrier",
+        categoryId: 1,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "Une lettre ou des documents qui arrivent par la poste.",
+        usage: "Tu dois envoyer ce courrier avant le 18 mai."
+    },
+    {
+        id: 203,
+        name: "un timbre",
+        categoryId: 1,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "On le colle sur une enveloppe ou sur un document officiel.",
+        usage: "Sur ce document il faut coller un timbre fiscal."
+    },
+    {
+        id: 204,
+        name: "un dossier",
+        categoryId: 1,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "Une pochette en carton ou en plastique pour ranger ensemble plusieur papiers et documents",
+        usage: "Tous les documents de la banque sont dans le même dossier, le bleu."
+    },
+    {
+        id: 205,
+        name: "un portefeuille",
+        categoryId: 1,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "Un petit sac pour ranger et transporter ses papier, ses cartes",
+        usage: "Mes photos d'identité sont dans mon portefeuille."
+    },
+    {
+        id: 206,
+        name: "un agenda",
+        categoryId: 1,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "Un cahier ou sur un téléphone. Il y a les jours et les dates, et ça sert à noter des rendez-vous.",
+        usage: "Elle note le rendez-vous sur son agenda."
+    },
 ]
 
 // Cat3: Voyager
 export const VOCABULARY_3: Vocabulary[] = [
-{
-    id: 301,
-    name: "un contrôleur",
-    categoryId: 1,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "Une personne qui contrôle les tickets dans le train, le bus ou le métro.",
-    usage: "Le contrôleur lui a demnadé son ticket."
-},
-{
-    id: 302,
-    name: "un carrefour",
-    categoryId: 1,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "Quand dexu route se croisent.",
-    usage: "La pharmacie est au carrefour entre ces deux rues."
-},
+    {
+        id: 301,
+        name: "un contrôleur",
+        categoryId: 1,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "Une personne qui contrôle les tickets dans le train, le bus ou le métro.",
+        usage: "Le contrôleur lui a demnadé son ticket."
+    },
+    {
+        id: 302,
+        name: "un carrefour",
+        categoryId: 1,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "Quand deux routes se croisent.",
+        usage: "La pharmacie est au carrefour entre ces deux rues."
+    },
 ]
 
 // Cat4 - Logement
 export const VOCABULARY_4: Vocabulary[] = [
-{
-    id: 401,
-    name: "un chiffon",
-    categoryId: 4,
-    audioSrc: "",
-    attr: "n. ",
-    explanation: "Un tissu pour faire le ménage.",
-    usage: "Enlève la poussière avec un chiffron."
-},
-{
-    id: 402,
-    name: "une éponge",
-    categoryId: 4,
-    attr: "n. ",
-    audioSrc: "",
-    explanation: "Un outil pour nettoyer la vaisselle.",
-    usage: "On a fini de manger. Il passe l'éponge pour nettoyer la table."
-},
-{
-    id: 403,
-    name: "une valise",
-    categoryId: 4,
-    attr: "n. ",
-    audioSrc: "",
-    explanation: "Une malle pour ranger ses affaires quand on voyage.",
-    usage: "N'oublie pas ta valise dans le train."
-},
-{
-    id: 404,
-    name: "un tiroir",
-    categoryId: 4,
-    attr: "n. ",
-    audioSrc: "",
-    explanation: "Dans un tiroir on range des papiers ou des vêtements par exemple.",
-    usage: "Ce bureau a trois tiroirs."
-},
-{
-    id: 405,
-    name: "la poubelle de tri",
-    categoryId: 4,
-    attr: "n. ",
-    audioSrc: "",
-    explanation: "Une poubelle où on jette les déchets qui peuvent être recyclés.",
-    usage: "Les légumes vont dans cette poubelleet le carton dans la poubelle du tri."
-},
+    {
+        id: 401,
+        name: "un chiffon",
+        categoryId: 4,
+        audioSrc: "",
+        attr: "n. ",
+        explanation: "Un tissu pour faire le ménage.",
+        usage: "Enlève la poussière avec un chiffron."
+    },
+    {
+        id: 402,
+        name: "une éponge",
+        categoryId: 4,
+        attr: "n. ",
+        audioSrc: "",
+        explanation: "Un outil pour nettoyer la vaisselle.",
+        usage: "On a fini de manger. Il passe l'éponge pour nettoyer la table."
+    },
+    {
+        id: 403,
+        name: "une valise",
+        categoryId: 4,
+        attr: "n. ",
+        audioSrc: "",
+        explanation: "Une malle pour ranger ses affaires quand on voyage.",
+        usage: "N'oublie pas ta valise dans le train."
+    },
+    {
+        id: 404,
+        name: "un tiroir",
+        categoryId: 4,
+        attr: "n. ",
+        audioSrc: "",
+        explanation: "Dans un tiroir on range des papiers ou des vêtements par exemple.",
+        usage: "Ce bureau a trois tiroirs."
+    },
+    {
+        id: 405,
+        name: "la poubelle de tri",
+        categoryId: 4,
+        attr: "n. ",
+        audioSrc: "",
+        explanation: "Une poubelle où on jette les déchets qui peuvent être recyclés.",
+        usage: "Les légumes vont dans cette poubelle et le carton dans la poubelle du tri."
+    },
 ]
 
 // Cat5: Activités et sortir
 export const VOCABULARY_5: Vocabulary[] = [
-{
-    id: 501,
-    name: "un musée",
-    categoryId: 5,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "Un endroit qu'on vosite pour voir des oeuvres d'art comme des peintures ou des sculptures.",
-    usage: "L'entrée du musée est gratuite tous les premier dimanches du mois."
-},
-{
-    id: 502,
-    name: "un chantier",
-    categoryId: 5,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "Un lieu où on fait des travaux, par exemple dans la rue pour constuire un immeuble.",
-    usage: "Le chantier qui est en face de chez mois est bruyant."
-},
+    {
+        id: 501,
+        name: "un musée",
+        categoryId: 5,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "Un endroit qu'on vosite pour voir des oeuvres d'art comme des peintures ou des sculptures.",
+        usage: "L'entrée du musée est gratuite tous les premier dimanches du mois."
+    },
+    {
+        id: 502,
+        name: "un pot",
+        categoryId: 5,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "(Familier) Réunion où l'on boit",
+        usage: "Il est invité à un pot d'anciens élèves."
+    },
+    {
+        id: 503,
+        name: "un chantier",
+        categoryId: 5,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "Un lieu où on fait des travaux, par exemple dans la rue pour constuire un immeuble.",
+        usage: "Le chantier qui est en face de chez mois est bruyant."
+    },
 ]
 
 // Cat6: Santé & Bien-être
 export const VOCABULARY_6: Vocabulary[] = [
-{
-    id: 601,
-    name: "un cabinet médical",
-    categoryId: 6,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "Le bureau où on peut consulter un médecin.",
-    usage: "Dans ce cabinet, il y a deux médecins qui travaillent."
-},
-{
-    id: 602,
-    name: "des lunettes",
-    categoryId: 6,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "On en porte pour mieux voir ou se protéger les yeux du soleil.",
-    usage: "Elle a besoin de lunettes pour lire."
-},
-{
-    id: 603,
-    name: "un thermomètre",
-    categoryId: 6,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "Outil pour mesurer la température",
-    usage: "Le thermomètre indique 38 degres; elle a de la fièvre."
-},
-{
-    id: 604,
-    name: "la sérologie",
-    categoryId: 6,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "La sérologie est un examen du sérum sanguin. Il consiste à détecter et étudier les anticorps correspondant à une maladie spécifique présents dans le sérum.",
-    usage: "Sérologie hépatite b; Sérologie VIH"
-},
-{
-    id: 605,
-    name: "Doliprane",
-    categoryId: 6,
-    attr: "n.",
-    audioSrc: "",
-    explanation: "C'est un médicament qui contient du paracétamol, utilisé pour traiter la douleur et la fièvre.",
-    usage: "Doliprane (Paracétamol), 500mg, 1 boîte, 16 comprimés"
-},
+    {
+        id: 601,
+        name: "un cabinet médical",
+        categoryId: 6,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "Le bureau où on peut consulter un médecin.",
+        usage: "Dans ce cabinet, il y a deux médecins qui travaillent."
+    },
+    {
+        id: 602,
+        name: "des lunettes",
+        categoryId: 6,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "On en porte pour mieux voir ou se protéger les yeux du soleil.",
+        usage: "Elle a besoin de lunettes pour lire."
+    },
+    {
+        id: 603,
+        name: "un thermomètre",
+        categoryId: 6,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "Outil pour mesurer la température",
+        usage: "Le thermomètre indique 38 degres; elle a de la fièvre."
+    },
+    {
+        id: 604,
+        name: "la sérologie",
+        categoryId: 6,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "La sérologie est un examen du sérum sanguin. Il consiste à détecter et étudier les anticorps correspondant à une maladie spécifique présents dans le sérum.",
+        usage: "Sérologie hépatite b; Sérologie VIH"
+    },
+    {
+        id: 605,
+        name: "Doliprane",
+        categoryId: 6,
+        attr: "n.",
+        audioSrc: "",
+        explanation: "C'est un médicament qui contient du paracétamol, utilisé pour traiter la douleur et la fièvre.",
+        usage: "Doliprane (Paracétamol), 500mg, 1 boîte, 16 comprimés"
+    },
 ]
 
 // Cat7: Vie amoureuse
@@ -319,20 +328,31 @@ export const VOCABULARY_7: Vocabulary[] = [
 
 // Cat8: Vie professionnelle
 export const VOCABULARY_8: Vocabulary[] = [
-{
-    id: 801,
-    name: "",
-    categoryId: 8,
-    audioSrc: "",
-    explanation: "",
-    usage: ""
-},
-{
-    id: 802,
-    name: "",
-    categoryId: 8,
-    audioSrc: "",
-    explanation: "",
-    usage: ""
-},
+    {
+        id: 801,
+        name: "RH",
+        categoryId: 8,
+        audioSrc: "",
+        attr: "n.",
+        explanation: "Ressources Humaines, en anglais: HR",
+        usage: "Vous serez contacté par nos équipes RH."
+    },
+    {
+        id: 802,
+        name: "un entretien (d'embauche)",
+        categoryId: 8,
+        audioSrc: "",
+        attr: "n.",
+        explanation: "Rendez-vous fixé pour avoir une conversation (probablement entre un candidat et un recruteur)",
+        usage: "Dans l'entretien, ne donnez pas aux recruteurs une information inutile."
+    },
+    {
+        id: 803,
+        name: "un CV",
+        categoryId: 8,
+        audioSrc: "",
+        attr: "n.",
+        explanation: "En latin: Curriculum Vitae (carrière de la vie), c'est un dossier comportant des informations essentielles d'un candidat à un poste.",
+        usage: "Pour éviter tout soucis de compatibilité, enregistrez votre CV au format PDF."
+    },
 ]
